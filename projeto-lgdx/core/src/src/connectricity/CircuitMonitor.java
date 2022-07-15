67558c65c63f076011844d10a9e706d4f651b5a8
Filipe Franco <f251027@dac.unicamp.br>
2022-07-15 01:10:45 -0300

    Comentarios em CircuitMonitor e Controller

--- projeto-lgdx/core/src/src/connectricity/CircuitMonitor.java
+++ projeto-lgdx/core/src/src/connectricity/CircuitMonitor.java
@@ -14,6 +14,15 @@ public class CircuitMonitor {
     }
 
     private void samePotential(int xIndex, int yIndex, String potential){
+    /**
+     * Recebe:
+     * a posicao x que esta sendo analizada
+     * a posicao y que esta sendo analizada
+     * o index de potencial determinado para aquela regiao
+     * 
+     * Recursivamente preenche uma regiao de condutores adjacentes do model com o mesmo index de potencial
+     */
+        
         model[yIndex][xIndex] = potential;
         if (yIndex > 0 && conductors.contains(model[yIndex - 1][xIndex])) {
             samePotential(xIndex, yIndex - 1, potential);
@@ -30,7 +39,16 @@ public class CircuitMonitor {
     }
 
     private int determinatePotentials() {
-        this.model = map.getCircuit();
+    /**
+     * Retorna o numero de posicoes de mesmo potencial
+     * 
+     * Percorre o model do circuito a procura de condutores
+     * Quando um condutor sem um index de potencial determidado eh encontrado, eh chamado o metodo samePotential()
+     * Esse condutor tera seu index potencial determinado, assim como todos os condutores adjacentes a ele
+     * Assim, ao final do metodo, o model tera varias regioes de condutores conectados com mesmo index de potencial
+     * Cada regiao separada possui um index diferente
+     */
+        this.model = map.getCircuit(); 
         int potentialIndex = 0;
         for (int yIndex = 0; yIndex < size[1]; yIndex++) {
             for (int xIndex = 0; xIndex < size[0]; xIndex++) {
@@ -44,6 +62,10 @@ public class CircuitMonitor {
     }
 
     public void contabilizeResistor(int[][] resistanceGraph, List<Integer> resistor) {
+    /**
+     * Recebe uma lista de inteiros que representa os index de potenciais adjacentes a um resistor
+     * A partir disso, atualiza a matriz que representa o grafo de resistores conectando as regioes de mesmo potencial
+     */
 
         for (int potentialIndex = 0; potentialIndex < resistanceGraph.length; potentialIndex++) {
             if (resistor.contains(potentialIndex)) {
@@ -60,6 +82,20 @@ public class CircuitMonitor {
     }
 
     private int[][] resistorConnections(int numberOfRegions, ArrayList<String> potentials) {
+    /**
+     * Recebe o numero de regioes de condutores com mesmo potencial e uma lista de strings com os index de potencial
+     * Retorna a matriz com o numero de resistores entre cada regiao
+     * 
+     * Cria uma matriz de inteiros, que faz o papel de grafo por matriz de incidencia
+     * Cada linha diz respeito a um index de potencial, ou seja, uma regiao de condutores
+     * Os index sao os mesmos do numero da linha que diz respeito a eles
+     * A partir disso, cada posicao nessa linha representa o numero de resistores que conectam a regiao da linha a regiao da posicao
+     * 
+     * Isso eh feito percorrendo o modelo a procura de resistores
+     * Quando um resistor eh encontrado eh criada uma lista de inteiros representando os index das regioes adjacentes a ele
+     * Apos o resistor ser preenchido com todas as regioes adjacentes, ele eh contabilizado na matriz
+     * Quando o model eh percorrido todos os resistores sao contabilizados e a matriz eh retornada
+     */
         int size[] = map.getSize();
         int[][] resistanceGraph = new int[numberOfRegions][numberOfRegions];
 
@@ -100,6 +136,17 @@ public class CircuitMonitor {
     }
 
     private void countResistorPath(Queue<Integer> queue, int[] queued, int[] resistorPath, int[][] connections, int currentPotential) {
+   /**
+     * Recebe:
+     * uma fila com os index de potencial a serem examinados, 
+     * um array que representa se os index de potenciais ja foram adicionados a fila
+     * um array com o menor numero de resistores entre o gerador examinado e cada index de potencial
+     * a matriz de conexoes com o numero de resistores entre cada regiao
+     * um inteiro com o index potencial que esta sendo analizado
+     * 
+     * Recursivamente conta o menor numero de resistores entre o gerador examinado e cada regiao, atualizando o resistorPath
+     * Funciona de maneira similar a um breadth first search
+     */
         for (int i = 0; i < connections[currentPotential].length; i++) {
             if (connections[currentPotential][i] > 0 && queued[i] == 0){
                 resistorPath[i] = resistorPath[currentPotential] + 1;
@@ -116,11 +163,23 @@ public class CircuitMonitor {
 
 
     private int[] determinePotentials(int numberOfRegions, int[][] connections, int[] generatorPosition) {
+    /**
+     * Recebe:
+     * um inteiro com o numero de regioes
+     * a matriz de conexoes com o numero de resistores entre cada regiao
+     * um array de tamanho 2 com as coordenadas do gerador que esta sendo analizado
+     * 
+     * Retorna um array de inteiros com o potencial de cada regiao de acordo que este gerador
+     * Cada posicao no array eh o index atribuido aquela regiao e o valor da posicao eh o valor do potencial dela
+     * 
+     * Chama o metodo countResistorPath() para determinar o menor numero de resistores entre o gerador e cada regiao
+     * Determina o potencial de cada regiao como 3 - numero minimo de resistores no caminho
+     */
         int generatorPotentialIndex = Integer.parseInt(model[generatorPosition[1]][generatorPosition[0]]);
-        int[] queued = new int[numberOfRegions];
-        int[] resistorPath = new int[numberOfRegions];
-        int[] potentialValues = new int[numberOfRegions];
-        Queue<Integer> queue = new LinkedList<Integer>();
+        int[] queued = new int[numberOfRegions]; // representa se os index de potenciais ja foram adicionados a fila
+        int[] resistorPath = new int[numberOfRegions]; // o menor numero de resistores entre o gerador examinado e cada index de potencial
+        int[] potentialValues = new int[numberOfRegions]; // o potencial de cada regiao, ordenado pelo index
+        Queue<Integer> queue = new LinkedList<Integer>(); //fila com os index de potencial a serem visitados
 
         for (int i = 0; i < numberOfRegions; i++) {
             queued[i] = 0;
@@ -141,9 +200,19 @@ public class CircuitMonitor {
     }
 
     private int[] determineFinalPotentials(int numberOfRegions,  int[][] connections) {
+    /**
+     * Recebe:
+     * um inteiro com o numero de regioes
+     * a matriz de conexoes com o numero de resistores entre cada regiao
+     * 
+     * Retorna um array com o potencial final de cada regiao de condutores
+     * 
+     * Determina o potencial parcial de cada regiao a partir de cada gerador
+     * Determina que o potencial final de cada regiao eh o maior potencial dado a ela por qualquer gerador
+     */
         int[] partialPotentials = new int[numberOfRegions];
         int[] finalPotentials = new int[numberOfRegions];
-        ArrayList<int[]> generatorPositions = map.getGeneratorPositions();
+        ArrayList<int[]> generatorPositions = map.getGeneratorPositions(); // lista com as posicoes de todos os geradores
 
         for (int i = 0; i < numberOfRegions; i++) {
             finalPotentials[i] = 0;
@@ -162,6 +231,15 @@ public class CircuitMonitor {
     }
 
     private int[][] createPotentialMatrix(int numberOfRegions, ArrayList<String> potentials,  int[][] connections) {
+    /**
+     * Recebe:
+     * um inteiro com o numero de regioes
+     * uma lista de Strings com os index de potenciais
+     * a matriz de conexoes com o numero de resistores entre cada regiao
+     * 
+     * Retorna uma matriz de inteiros com o potencial de cada condutor
+     * Caso nao haja condutores na posicao, seu potencial eh dado como -1
+     */
         int[][] potentialMatrix = new int[size[1]][size[0]];
         int[] finalPotentials = determineFinalPotentials(numberOfRegions, connections);
 
@@ -179,6 +257,10 @@ public class CircuitMonitor {
     }
 
     private ArrayList<String> createPotentialsString(int numberOfRegions) {
+    /**
+     * Recebe o numero de regioes de condutores
+     * Retorna lista de Strings com todos os index de potencial usados
+     */
         ArrayList<String> potentials = new ArrayList<String>();
         for (int i = 0; i < numberOfRegions; i++) {
             potentials.add(Integer.toString(i));
@@ -187,6 +269,9 @@ public class CircuitMonitor {
     }
 
     public void setPotentials() {
+    /**
+     * Gera a matriz de potenciais finais de cada condutor e chama o metodos setPotentialLevel para cada um deles
+     */
         int numberOfRegions = determinatePotentials();
         ArrayList<String> potentials = createPotentialsString(numberOfRegions);
         int[][] connections = resistorConnections(numberOfRegions, potentials);
@@ -202,6 +287,9 @@ public class CircuitMonitor {
     }
 
     public void printarMatriz(int[][] matriz){
+    /**
+     * Faz print de uma matriz de inteiros para facilitar debugging
+     */
         for(int i = 0; i < matriz.length; i++) {
             System.out.print("\t");
             for (int j = 0; j < matriz[0].length; j++){
@@ -212,6 +300,9 @@ public class CircuitMonitor {
     }
 
     public String printarLista(int[] lista) {
+    /**
+     * Faz print de uma lista para facilitar debugging
+     */
         String l = "";
         for(int i = 0; i < lista.length; i++) {
             l += lista[i] + " ";
--- projeto-lgdx/core/src/src/connectricity/Controller.java
+++ projeto-lgdx/core/src/src/connectricity/Controller.java
@@ -13,6 +13,10 @@ public class Controller {
     }
 
     public void receiveCommand(String key){
+    /**
+     * Recebe uma string com a tecla pressionada pelo jogador
+     * Executa o método correto a partir do comando utilizado
+     */
         int xIndex = player.getPosition()[0];
         int yIndex = player.getPosition()[1];
 
@@ -57,6 +61,10 @@ public class Controller {
     }
 
     public void move(int destSquare_x, int destSquare_y, Square curSquare){
+      /**
+     * Se possivel, realiza o movimento do jogador. 
+     * Tambem checa se o jogador atingiu uma saida aberta para ganhar o jogo
+     */
 
         if (map.invalidMove(destSquare_x, destSquare_y)) {
             System.out.println("MOVIMENTO INVALIDO!");
@@ -78,6 +86,9 @@ public class Controller {
     }
 
     public void takeWire(Square curSquare) {
+    /**
+     * Se possivel, retira um fio da posicao atual e o coloca no inventario do jogador.
+     */
         if (curSquare.checkWire() && player.hasWireSpace()) {
             Wire wire = curSquare.removeWire();
             player.storeWire(wire);
@@ -88,6 +99,9 @@ public class Controller {
     }
 
     public void placeWire(Square curSquare) {
+    /**
+     * Se possivel, retira um fio do inventário do jogador e o coloca na posicao atual
+     */
         if (curSquare.emptySquare()) {
             Wire wire = player.spendWire();
             if (wire != null) {
@@ -103,6 +117,9 @@ public class Controller {
     }
 
     public void takeResistor(Square curSquare) {
+    /**
+     * Se possivel, retira um resistor da posicao atual e o coloca no inventario do jogador.
+     */
         if (curSquare.checkResistor() && player.hasResistorSpace()) {
             Resistor resistor = curSquare.removeResistor();
             player.storeResistor(resistor);
@@ -113,6 +130,9 @@ public class Controller {
     }
 
     public void placeResistor(Square curSquare) {
+    /**
+     * Se possivel, retira um resistor do inventário do jogador e o coloca na posicao atual
+     */
         if (curSquare.emptySquare()) {
             Resistor resistor = player.spendResistor();
             if (resistor != null) {
